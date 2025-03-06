cfebd6230db3982979a6d5bc32a395d431f25f6f
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-06 12:29:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient, ASCENDING, DESCENDING
 
 # MongoDB Connection
-DATABASE_URL = "mongodb+srv://Altor:matrix7bonten@cluster0.izvyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URL = "mongodb+srv://Play:music@cluster0.izvyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(DATABASE_URL)
 db = client["wordmine"]
 
