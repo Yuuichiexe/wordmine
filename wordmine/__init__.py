d209361a0e9f317ae27489a1df4f6f33c9f6e49a
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-06 23:09:09 +0530

    Update __init__.py

--- wordmine/__init__.py
+++ wordmine/__init__.py
@@ -9,3 +9,6 @@ API_HASH = os.getenv("API_HASH", "5788f1f4a93f2de28835a0cf1b0ebae4")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6694970760:AAGe7pAorvORM7xRb-9AyOpdoEfNJMd6Ric")
 
 app = Client("wordmine", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+print(pyrogram.__version__)
+
