a9720a97559a9c1b6348c5ca8c782bd7e0c95bed
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-07 22:03:24 +0530

    Update __init__.py

--- wordmine/__init__.py
+++ wordmine/__init__.py
@@ -6,9 +6,9 @@ OWNER_ID = 6058139652
 
 API_ID = int(os.getenv("API_ID", "20222660"))
 API_HASH = os.getenv("API_HASH", "5788f1f4a93f2de28835a0cf1b0ebae4")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6694970760:AAGe7pAorvORM7xRb-9AyOpdoEfNJMd6Ric")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7560532835:AAHOYKZhGS7rR4vVdQBNWxre0-1GV7SShXQ")
 
-app = Client("wordmine", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+app = Client("word_mine", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
 
 
